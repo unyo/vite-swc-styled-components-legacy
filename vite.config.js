ad9f73d81660843ecfc38d4a8b26f2bd8f5df2e5
Cody Moniz <cody.moniz@gmail.com>
2024-02-01 07:44:29 -1000

    Might as well add in the comments

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,11 @@ export default defineConfig(({ mode }) => ({
     legacy(),
   ],
   define: {
-    'process.env.NODE_ENV': `'${mode || 'production'}'`,
-    'SC_DISABLE_SPEEDY': "true",
+    'process.env.NODE_ENV': `'${mode || 'production'}'`, // browser throws an error about process not defined on determineTheme.ts
+    'SC_DISABLE_SPEEDY': "true", // needed to enable vendor prefixing using 'vite build'
+    // using process.env.SC_DISABLE_SPEEDY doesnt work due to the way styled-components checks for process.env 
+    // https://github.com/styled-components/styled-components/blob/main/packages/styled-components/src/constants.ts#L17
+    // I'm guessing process.env ternary worked in webpack but doesn't work with vite
+    // you also need to wrap the app with <StyleSheetManager enableVendorPrefixes={true}>
   }
 }))
